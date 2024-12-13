68f44a28cd91e8ca9027947e867143e20fdc1faa
khushi4243 <khushi108@icloud.com>
2024-12-13 10:47:26 -0500

    cognito setup

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,6 +6,6 @@ export const cognitoDomainName = "eoed-auth"
 // this can be anything that would be understood easily, but you must use the same name
 // when setting up a sign-in provider in Cognito
 // make sure to leave it blank if you do not actually have an SSO provider configured in Cognito!
-export const OIDCIntegrationName = ""
+export const OIDCIntegrationName = "EOED-BFD-Beacon-AI-Sandbox"
 // this MUST be unique to your account
 export const stackName = "EOEDStack"
